5a5af2877254f952bbca48118850fa440493bd89
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2024-01-23 03:46:35 +0900

    Create addPoint function to increase given player points

--- src/components/kata/Tennis/TennisScore.vue
+++ src/components/kata/Tennis/TennisScore.vue
@@ -4,6 +4,14 @@ import {ref} from "vue";
 let scorePlayerOne = ref('love');
 let scorePlayerTwo = ref('love');
 
+const addPoint = player => {
+  const availablePoints = ['love', '15', '30', '40'];
+  let playerScore = player === 1 ? scorePlayerOne : scorePlayerTwo
+
+  let nextIndex = availablePoints.indexOf(playerScore.value) + 1
+  playerScore.value = availablePoints[nextIndex]
+}
+
 </script>
 
 <template>
--- src/components/kata/__tests__/TennisScore.spec.ts
+++ src/components/kata/__tests__/TennisScore.spec.ts
@@ -14,4 +14,19 @@ describe('TennisScore', () => {
     expect(wrapper.getComponent(TennisScore).vm.scorePlayerOne).toBe('love')
     expect(wrapper.getComponent(TennisScore).vm.scorePlayerTwo).toBe('love')
   })
+
+  describe('add points', () => {
+    it('goes from 0 to 40', () => {
+      const wrapper = mount(TennisScore)
+      wrapper.getComponent(TennisScore).vm.addPoint(1)
+      expect(wrapper.getComponent(TennisScore).vm.scorePlayerOne).toBe('15')
+
+      wrapper.getComponent(TennisScore).vm.addPoint(1)
+      expect(wrapper.getComponent(TennisScore).vm.scorePlayerOne).toBe('30')
+
+      wrapper.getComponent(TennisScore).vm.addPoint(1)
+      expect(wrapper.getComponent(TennisScore).vm.scorePlayerOne).toBe('40')
+    })
+  })
+  })
 })
